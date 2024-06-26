e394e6c967632ab45d87ac8eee7e1ab1d59c4639
fmelods <felipemelodesousa4@gmail.com>
2024-06-26 20:12:12 -0300

    Conversor Celsius, Farenheit e Kelvin

---
+++ conversor/Form1.Designer.vb
@@ -0,0 +1,276 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        tC = New TextBox()
+        tF = New TextBox()
+        tK = New TextBox()
+        tF2 = New TextBox()
+        tC2 = New TextBox()
+        tK2 = New TextBox()
+        tK3 = New TextBox()
+        tC3 = New TextBox()
+        tF3 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(116, 31)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(190, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Insira uma temperatura em Celsius"
+        Label1.TextAlign = ContentAlignment.TopCenter
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(116, 62)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(138, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Convertido em Farenheit"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(116, 91)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(121, 15)
+        Label3.TabIndex = 2
+        Label3.Text = "Convertido em Kelvin"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(116, 223)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(121, 15)
+        Label4.TabIndex = 5
+        Label4.Text = "Convertido em Kelvin"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(116, 191)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(126, 15)
+        Label5.TabIndex = 4
+        Label5.Text = "Convertido em Celsius"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(116, 159)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(202, 15)
+        Label6.TabIndex = 3
+        Label6.Text = "Insira uma temperatura em Farenheit"
+        Label6.TextAlign = ContentAlignment.TopCenter
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(116, 342)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(138, 15)
+        Label7.TabIndex = 8
+        Label7.Text = "Convertido em Farenheit"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(116, 314)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(126, 15)
+        Label8.TabIndex = 7
+        Label8.Text = "Convertido em Celsius"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(116, 285)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(185, 15)
+        Label9.TabIndex = 6
+        Label9.Text = "Insira uma temperatura em Kelvin"
+        Label9.TextAlign = ContentAlignment.TopCenter
+        ' 
+        ' tC
+        ' 
+        tC.Location = New Point(321, 28)
+        tC.Name = "tC"
+        tC.Size = New Size(100, 23)
+        tC.TabIndex = 9
+        ' 
+        ' tF
+        ' 
+        tF.Location = New Point(321, 59)
+        tF.Name = "tF"
+        tF.Size = New Size(100, 23)
+        tF.TabIndex = 10
+        ' 
+        ' tK
+        ' 
+        tK.Location = New Point(321, 91)
+        tK.Name = "tK"
+        tK.Size = New Size(100, 23)
+        tK.TabIndex = 11
+        ' 
+        ' tF2
+        ' 
+        tF2.Location = New Point(324, 159)
+        tF2.Name = "tF2"
+        tF2.Size = New Size(100, 23)
+        tF2.TabIndex = 12
+        ' 
+        ' tC2
+        ' 
+        tC2.Location = New Point(324, 191)
+        tC2.Name = "tC2"
+        tC2.Size = New Size(100, 23)
+        tC2.TabIndex = 13
+        ' 
+        ' tK2
+        ' 
+        tK2.Location = New Point(324, 223)
+        tK2.Name = "tK2"
+        tK2.Size = New Size(100, 23)
+        tK2.TabIndex = 14
+        ' 
+        ' tK3
+        ' 
+        tK3.Location = New Point(324, 285)
+        tK3.Name = "tK3"
+        tK3.Size = New Size(100, 23)
+        tK3.TabIndex = 15
+        ' 
+        ' tC3
+        ' 
+        tC3.Location = New Point(324, 314)
+        tC3.Name = "tC3"
+        tC3.Size = New Size(100, 23)
+        tC3.TabIndex = 16
+        ' 
+        ' tF3
+        ' 
+        tF3.Location = New Point(324, 343)
+        tF3.Name = "tF3"
+        tF3.Size = New Size(100, 23)
+        tF3.TabIndex = 17
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(29, 28)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(81, 74)
+        Button1.TabIndex = 18
+        Button1.Text = "CONVERTER"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(29, 159)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(81, 71)
+        Button2.TabIndex = 19
+        Button2.Text = "CONVERTER"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(29, 285)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(81, 72)
+        Button3.TabIndex = 20
+        Button3.Text = "CONVERTER"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(453, 405)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(tF3)
+        Controls.Add(tC3)
+        Controls.Add(tK3)
+        Controls.Add(tK2)
+        Controls.Add(tC2)
+        Controls.Add(tF2)
+        Controls.Add(tK)
+        Controls.Add(tF)
+        Controls.Add(tC)
+        Controls.Add(Label7)
+        Controls.Add(Label8)
+        Controls.Add(Label9)
+        Controls.Add(Label4)
+        Controls.Add(Label5)
+        Controls.Add(Label6)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Conversor de Temperatura"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents tC As TextBox
+    Friend WithEvents tF As TextBox
+    Friend WithEvents tK As TextBox
+    Friend WithEvents tF2 As TextBox
+    Friend WithEvents tC2 As TextBox
+    Friend WithEvents tK2 As TextBox
+    Friend WithEvents tK3 As TextBox
+    Friend WithEvents tC3 As TextBox
+    Friend WithEvents tF3 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+
+End Class
---
+++ conversor/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        tF.Text = ((9 / 5) * Val(tC.Text) + 32).ToString()
+        tK.Text = (273.15 + Val(tC.Text)).ToString()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        tC2.Text = ((Val(tF2.Text) - 32) * 5 / 9).ToString()
+        tK2.Text = ((Val(tF2.Text) - 32) * 5 / 9 + 273.15).ToString()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        tC3.Text = (Val(tK3.Text) - 273.15).ToString()
+        tF3.Text = ((Val(tK3.Text) - 273.15) * 9 / 5 + 32).ToString()
+    End Sub
+End Class
